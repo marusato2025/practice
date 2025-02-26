6f132dfec911cf8827d067a743b7aba6ad5d7336
marusato2025 <kougamarusato1990@gmail>
2025-02-26 19:14:59 +0900

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,11 @@
+[200~public class Book extends TangibleAsset {
+	private String isbn;
+	public Book(String name,int price,String color,String isbn) {
+			//			super(name,price,color);
+			//					this.isbn =isbn;
+			//						}
+			//							//メソッド
+			//								public String getIsbn() {
+			//										return this.isbn;
+			//											}
+			//											}
